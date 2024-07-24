d78462f178c047f771537b0fa880cb2a83d642dd
unknown <fuaad29@gmail.com>
2024-07-24 18:07:47 +0500

    forntend validations added

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -30,49 +30,48 @@ const Login = () => {
   };
 
   return (
-    <div className='bg-gray-300 p-12 rounded-xl w-1/2 flex flex-col justify-center items-center m-auto'>
-      <h2 className='font-bold text-2xl mb-16'>Login</h2>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: 'Please input your email!' }]}
+    <div className='flex justify-center items-center h-screen bg-gray-600'>
+      <div className='bg-gray-300 p-12 rounded-xl w-1/3 flex flex-col justify-center items-center m-auto'>
+        <h2 className='font-bold text-2xl mb-16'>Login</h2>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-      <span className="text-sm text-black">Not registered yet? <a className='cursor-pointer' onClick={() => navigate("/register")}>Register Now</a></span>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        <span className="text-sm text-black">Not registered yet? <a className='cursor-pointer text-blue-600 font-bold' onClick={() => navigate("/register")}>Register Now</a></span>
+      </div>
     </div>
   );
 };
--- src/Components/Authentication/Register.jsx
+++ src/Components/Authentication/Register.jsx
@@ -7,8 +7,16 @@ const Register = () => {
 
   const onFinish = (values) => {
     console.log("values:", values);
-    
-    (axios.post('http://127.0.0.1:8000/api/signup', values)
+    const {name, email, password} = values
+    const chars = /[@*&^%$#|":;,.]/
+    if (chars.test(name)) {
+      notification.error({message: "Name shouldn't contain any character"})
+    } else if (!email.includes("@")) {
+      notification.error({message: "Please enter a valid email"})
+    } else if (password.length < 8) {
+      notification.error({message: "Password should be at least 8 characters long"})
+    } else {
+      (axios.post('http://127.0.0.1:8000/api/signup', values)
       .then(response => {
         console.log('response:', response);
         navigate('/login')
@@ -22,8 +30,8 @@ const Register = () => {
           message: error.data.message,
           description: 'Please try again.',
         });
-      })
-    )
+      }))
+    }
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -31,57 +39,55 @@ const Register = () => {
   };
 
   return (
-    <div className='bg-gray-300 p-12 rounded-xl w-1/2 flex flex-col justify-center items-center m-auto'>
-      <h2 className='font-bold text-2xl mb-16'>Register</h2>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Name"
-          name="name"
-          rules={[{ required: true, message: 'Please input your name!' }]}
+    <div className='flex justify-center items-center h-screen bg-gray-600'>
+      <div className='bg-gray-300 p-12 rounded-xl w-1/3 flex flex-col justify-center items-center m-auto'>
+        <h2 className='font-bold text-2xl mb-16'>Register</h2>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: 'Please input your email!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        {/* <Form.Item
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item> */}
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-      <span className="text-sm text-black">Already registered <a className='cursor-pointer' onClick={() => navigate("/login")}>Login</a></span>
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: 'Please input your name!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          {/* <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item> */}
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        <span className="text-sm text-black">Already registered <a className='cursor-pointer text-blue-600 font-bold' onClick={() => navigate("/login")}>Login</a></span>
+      </div>
     </div>
   );
 };
