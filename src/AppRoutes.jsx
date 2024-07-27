8d572cf65e4d8e6633bb8bc640a896c56d91b265
unknown <fuaad29@gmail.com>
2024-07-27 20:23:25 +0500

    added Products page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -9,6 +9,7 @@ import ProductForm from './Components/Products/ProductForm'
 import Category from './Components/Categories/Categories'
 import CategoryPage from './Components/Categories/CategoryPage'
 import Categories from './Components/Categories/Categories'
+import Products from './Components/Products/Products'
 
 function AppRoutes() {
 
@@ -22,6 +23,8 @@ function AppRoutes() {
           <Route path="/categories" element={<Categories />} />
           <Route path="/categories/add_new" element={<CategoryForm />} />
           <Route path="/products/add_new" element={<ProductForm />} />
+          <Route path="/products" element={<Products />} />
+
 
 
           <Route exact path="category" component={Category} />
