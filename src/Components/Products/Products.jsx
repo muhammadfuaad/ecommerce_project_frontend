cfdb2bb7f364b25ebaca9308866c895a61835c69
unknown <fuaad29@gmail.com>
2024-07-29 21:40:48 +0500

    configured product component

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../../utils/callAxios";
 import './Styles.css';
-import { useNavigate } from "react-router";
+// import { useNavigate } from "react-router";
+import Product from "./Product";
 
 const Products = () => {
-  const navigate = useNavigate()
   const [products, setProducts] = useState([])
   useEffect(()=>{
     axiosInstance.get('/product').then((response)=>{
@@ -21,18 +21,7 @@ const Products = () => {
         <h2>RECOMMENDED FOR YOU</h2>
         <div className="grid">
           {products.map(product => (
-            <div key={product.id} className="product-card" onClick={()=>{navigate('/product', {state: product})}}>
-              <img src='./chairs_image.jpg' alt={product.name} />
-              <h3>{product.name}</h3>
-              <p className="price">
-                <span className="new-price">${product.price.toFixed(2)}</span>
-                {/* <span className="old-price">${product.originalPrice.toFixed(2)}</span> */}
-              </p>
-              <p className="rating">
-                {'★'.repeat(product.rating)} <span>({product.reviews})</span>
-              </p>
-              <button>Add to cart</button>
-            </div>
+            <Product key={product._id} product={product}/>
           ))}
         </div>
       </div>
